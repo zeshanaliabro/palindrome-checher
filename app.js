3c544e0091c1aecae6563c6cd6c0670518ceb74f
zeshan ali <zehsan290@gmail.com>
2025-01-23 10:28:42 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function check(){
+    let palindromewords= ["madam","mam","lebel","noon","level","refer","civic"];
+    // let palindrome1 = [].split("").reverse("").join("");
+   const palindromeList = document.getElementById("palindromeList").value;
+  const palindrome =  document.getElementById("li")
+  const reverseWord = palindromeList.split("").reverse().join("");
+  if(reverseWord === palindromeList){
+    palindrome.innerHTML = "this is palindrome words ";
+  }else{
+    palindrome.innerHTML = "this is not palindrome words ";
+  }
+  
+ 
+
+}
